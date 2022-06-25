689417d3a5fddf33315c7de1e59d49edcc961f68
José Rufino <joseaorufino@gmail.com>
2022-06-24 22:58:04 -0300

    tests with clojure using TDD

--- src/treinamento_clojure/core.clj
+++ src/treinamento_clojure/core.clj
@@ -1,6 +1,11 @@
 (ns treinamento-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn taxa-de-entrega
+  ;"Se o valor da compra for até 100, deve-se cobrar 15 reais de entrega.
+  ; Se for maior que 100 e menor que 200, deve-se cobrar 5 reais de entrega..
+  ; Se for acima de 200, a taxa é gratuita."
+  [valor-da-compra]
+  (cond (<= valor-da-compra 100) 15
+        (<= valor-da-compra 200) 5
+        :else 0))
+
--- test/treinamento_clojure/core_test.clj
+++ test/treinamento_clojure/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [treinamento-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest taxa-de-entrega-test
+  (testing "Valor da compra for até 100, deve-se cobrar 15 reais de entrega"
+    (is (= 15 (taxa-de-entrega 1)))
+    (is (= 15 (taxa-de-entrega 100))))
+  (testing "Valor da compra maior que 100 e até 200, deve-se cobrar 5 reais de entrega"
+    (is (= 5 (taxa-de-entrega 100.01)))
+    (is (= 5 (taxa-de-entrega 200))))
+  (testing "Valor da compra acima de 200, a taxa é gratuita"
+    (is (= 0 (taxa-de-entrega 200.01)))
+    (is (= 0 (taxa-de-entrega 201)))
+    (is (= 0 (taxa-de-entrega 34223)))))
+
+
